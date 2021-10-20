fae82fc7c4ec895d8708423be34431dab9ad0a60
theodorCodes <laphan.fan@gmail.com>
2021-10-20 17:05:32 +0800

    added import os in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 # IMPORT THIS FOR POSTGRES ON HEROKU (lp1)
 import dj_database_url
+# IMPORT OS
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
